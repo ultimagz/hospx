433ff68200ca393ceb5f547d4d87691c4c47a138
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2020-02-28 12:03:25 +0700

    Add data module

---
+++ data/build.gradle.kts
@@ -0,0 +1,44 @@
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(Apps.compileSdk)
+
+    defaultConfig {
+        minSdkVersion(Apps.minSdk)
+        targetSdkVersion(Apps.targetSdk)
+        versionCode = Apps.versionCode
+        versionName = Apps.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    sourceSets["main"].java.srcDir("src/main/kotlin")
+    sourceSets["test"].java.srcDir("src/test/kotlin")
+    sourceSets["androidTest"].java.srcDir("src/androidTest/kotlin")
+
+    sourceSets["test"].resources.srcDirs("src/resources")
+    sourceSets["androidTest"].resources.srcDirs("src/resources")
+}
+
+dependencies {
+    project(":domain")
+
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementation(Libs.kotlin)
+    testImplementation(TestLibs.junit)
+}
