4121fc5b8d88ed554d1054d63dac8f5174f84efd
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2020-03-02 16:29:53 +0700

    Change layout to binding

--- app/src/main/kotlin/gz/tar/ultimagz/hospx/SplashActivity.kt
+++ app/src/main/kotlin/gz/tar/ultimagz/hospx/SplashActivity.kt
@@ -1,12 +1,48 @@
 package gz.tar.ultimagz.hospx
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityOptionsCompat
+import gz.tar.ultimagz.hospx.databinding.ActivitySplashBinding
+import kotlinx.android.synthetic.main.activity_splash.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
 
+    private val binding: ActivitySplashBinding by lazy { ActivitySplashBinding.inflate(layoutInflater) }
+
+    var transitionJob: Job? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(binding.root)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        transitionToLogin()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val active = transitionJob?.isActive ?: false
+        if (active) {
+            transitionJob?.cancel()
+        }
+    }
+
+    private fun transitionToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        transitionJob = GlobalScope.launch {
+            delay(2000L)
+            startActivity(intent)
+            finish()
+        }
     }
 }
