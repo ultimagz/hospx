57289017d880b6f4d1c57d4d9a88d152f9cab8ed
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2020-03-02 16:30:22 +0700

    Add login activity

---
+++ app/src/main/kotlin/gz/tar/ultimagz/hospx/LoginActivity.kt
@@ -0,0 +1,16 @@
+package gz.tar.ultimagz.hospx
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import gz.tar.ultimagz.hospx.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private val binding: ActivityLoginBinding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        
+    }
+}
