b0cb693f6321f62865565b22ecb5a37bff55b740
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2020-03-05 18:50:22 +0700

    Set timber logger

--- app/src/main/kotlin/gz/tar/ultimagz/hospx/app/HospxApplication.kt
+++ app/src/main/kotlin/gz/tar/ultimagz/hospx/app/HospxApplication.kt
@@ -1,16 +1,25 @@
 package gz.tar.ultimagz.hospx.app
 
 import androidx.multidex.MultiDexApplication
+import gz.tar.ultimagz.hospx.BuildConfig
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
-class HospxApplication: MultiDexApplication() {
+class HospxApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidContext(this@HospxApplication)
             modules(listOf())
             logger(TimberLogger())
+        }
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        } else {
+//            Timber.plant(CrashReportingTree())
         }
     }
 }
