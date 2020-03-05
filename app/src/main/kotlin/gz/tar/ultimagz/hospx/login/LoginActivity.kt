9a4754ee12f10639fae20cab5608f2cdabb2c96d
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2020-03-05 18:51:24 +0700

    Set button login action

--- app/src/main/kotlin/gz/tar/ultimagz/hospx/login/LoginActivity.kt
+++ app/src/main/kotlin/gz/tar/ultimagz/hospx/login/LoginActivity.kt
@@ -1,7 +1,9 @@
 package gz.tar.ultimagz.hospx.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import gz.tar.ultimagz.hospx.category.SelectCategoryActivity
 import gz.tar.ultimagz.hospx.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -11,6 +13,9 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, SelectCategoryActivity::class.java))
+        }
     }
 }
